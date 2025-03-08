2b676bd3deba97bb7249f4e875c1a42c8fd5efc7
linuztx <linuztx@gmail.com>
2025-03-09 00:17:57 +0800

    feat: Admin only management buttons

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -99,12 +99,14 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                                     </svg>
                                 </button>
+                                @if(Auth::user()->isAdmin())
                                 <button onclick="showResetPasswordModal({{ $user->id }})"
                                         class="text-yellow-600 hover:text-yellow-800">
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"></path>
                                     </svg>
                                 </button>
+                                @endif
                                 @if(Auth::user()->canDeleteUsers() && Auth::id() !== $user->id)
                                     <button onclick="deleteUser({{ $user->id }})"
                                             class="text-red-600 hover:text-red-800">
